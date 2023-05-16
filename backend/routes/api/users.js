2df9cc5a4fa5f63e0d5b818a6486d6c74d8eedd2
Kim <kimharris2018@gmail.com>
2023-05-16 18:07:29 -0500

    signup endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,8 @@
-// test
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { check, validationResult } = require('express-validator');
 const { User } = require('../../db/models');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
@@ -12,46 +10,65 @@ const validateSignup = [
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Invalid email.'),
   check('username')
     .exists({ checkFalsy: true })
+    .withMessage('Username is required.')
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
+    .withMessage('Username must be at least 4 characters long.'),
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
     .withMessage('Password must be 6 characters or more.'),
-  handleValidationErrors
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('First Name is required.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Last Name is required.'),
 ];
-
-
 
 // Sign up
 router.post(
-  '',
+  '/',
   validateSignup,
   async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: 'Validation error', errors: errors.array() });
+    }
+
     const { firstName, lastName, email, password, username } = req.body;
-    const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
-    const safeUser = {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      username: user.username,
-    };
+    try {
+      let user = await User.findOne({ where: { email } });
+      if (user) {
+        return res.status(500).json({ message: 'User already exists', errors: { email: 'User with that email already exists' } });
+      }
 
-    await setTokenCookie(res, safeUser);
+      user = await User.findOne({ where: { username } });
+      if (user) {
+        return res.status(500).json({ message: 'User already exists', errors: { username: 'User with that username already exists' } });
+      }
 
-    return res.json({
-      user: safeUser
-    });
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user = await User.create({ firstName, lastName, email, username, hashedPassword });
+
+      const safeUser = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+      };
+
+      await setTokenCookie(res, safeUser);
+
+      return res.status(200).json({ user: safeUser });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
   }
 );
 
