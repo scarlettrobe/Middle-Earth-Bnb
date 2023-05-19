2eae2951572a53a2a8c0d10b16a8b0d3c0d9e74e
Kim <kimharris2018@gmail.com>
2023-05-19 17:41:39 -0500

    rollback

--- backend/db/seeders/20230518212006-spots.js
+++ backend/db/seeders/20230518212006-spots.js
@@ -1,9 +1,4 @@
 'use strict';
-const bcrypt = require("bcryptjs");
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
-}
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
