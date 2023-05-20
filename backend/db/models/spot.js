499f1d7aae33a06ec3df64dfc762f2bcae679fcf
Kim <kimharris2018@gmail.com>
2023-05-19 21:07:04 -0500

    changed spot model

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -1,4 +1,5 @@
 'use strict';
+// test
 const {
   Model
 } = require('sequelize');
@@ -10,55 +11,32 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-
-      Spot.belongsTo(models.User, {
-        foreignKey: 'ownerId',
-        as: 'Owner'
-      })
-
-      Spot.belongsToMany(models.User, {
-        through: models.Booking,
-        foreignKey: 'spotId',
-        otherKey: 'userId'
-      })
-
-      Spot.belongsToMany(models.User, {
-        through: models.Review,
-        foreignKey: 'spotId',
-        otherKey: 'userId'
-      })
-
-      Spot.hasMany(models.SpotImage, {
-        foreignKey: 'spotId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      Spot.hasMany(models.Review, {
-        foreignKey: 'spotId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      Spot.hasMany(models.Booking, {
-        foreignKey: 'spotId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
+      Spot.belongsTo(
+        models.User,
+          { foreignKey: 'ownerId' }
+      );
+      Spot.hasMany(
+        models.SpotImage,
+        {
+          foreignKey: 'spotId',
+          onDelete: 'CASCADE',
+        },
+      );
+      Spot.hasMany(
+        models.Review,
+        {
+          foreignKey: 'spotId',
+          onDelete: 'CASCADE',
+        },
+      ),
+      Spot.hasMany(
+        models.Booking,
+          { foreignKey: 'spotId' }
+      );
     }
   }
   Spot.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    ownerId: {
-      type: DataTypes.INTEGER,
-
-    },
+    ownerId: DataTypes.INTEGER,
     address: DataTypes.STRING,
     city: DataTypes.STRING,
     state: DataTypes.STRING,
@@ -66,16 +44,36 @@ module.exports = (sequelize, DataTypes) => {
     lat: DataTypes.DECIMAL,
     lng: DataTypes.DECIMAL,
     name: {
-      type: DataTypes.STRING,
-      allowNull: false
+      type: DataTypes.STRING(50),
+      validate: {
+        isOver50Chars(name) {
+          if (name.length > 50) {
+            throw new Error('Name must be less than 50 characters');
+          }
+        }
+      }
     },
     description: {
-      type: DataTypes.STRING,
-      allowNull: false
+      type: DataTypes.TEXT,
+      validate: {
+        isAtLeast30Chars(description){
+          if (description.length < 30) {
+            throw new Error("Description needs a minimum of 30 characters")
+          }
+        }
+      }
     },
-    price: DataTypes.DECIMAL,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
+    price: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        min: 1,
+        isPositiveInt(price) {
+          if (price < 0) {
+            throw new Error("Price is required")
+          }
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'Spot',
