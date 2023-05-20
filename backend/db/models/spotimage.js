d4c710c941317809a02472f0149cd7b89ad45a30
Kim <kimharris2018@gmail.com>
2023-05-19 21:54:43 -0500

    spot images model change

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -1,14 +1,28 @@
 'use strict';
-const { Model } = require('sequelize');
+// test
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class SpotImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      SpotImage.belongsTo(models.Spot, { foreignKey: 'spotId' });
+      // define association here
+      SpotImage.belongsTo(
+        models.Spot,
+          { foreignKey: 'spotId' }
+      );
     }
-  };
+  }
   SpotImage.init({
     spotId: DataTypes.INTEGER,
-    url: DataTypes.STRING,
+    url:{
+      type: DataTypes.STRING,
+    },
     preview: DataTypes.BOOLEAN
   }, {
     sequelize,
