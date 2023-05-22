c9883396466a42418fb13a4919eef924733da725
Kim <kimharris2018@gmail.com>
2023-05-22 06:24:40 -0500

    fixed /sessions to include first and last name

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -78,8 +78,11 @@ router.get(
       if (user) {
         const safeUser = {
           id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
           email: user.email,
           username: user.username,
+
         };
         return res.json({
           user: safeUser
