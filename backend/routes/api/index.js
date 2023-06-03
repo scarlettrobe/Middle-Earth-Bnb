51cc36ded5da6e521b61b79fac5e45c0ad0a5e21
Kim <kimharris2018@gmail.com>
2023-06-02 19:58:06 -0500

    auth me setup p1, might have some errors, will correct tomorrow morning

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,18 +9,16 @@ const bookingRouter = require('./bookings.js')
 const spotImageRouter = require('./spot-images.js')
 const reviewImageRouter = require('./review-images.js')
 
-// Connect restoreUser middleware to the API router
-// If current user session is valid, set req.user to the user in the database
-// If current user session is not valid, set req.user to null
+
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/spots', spotRouter) //added here
+router.use('/spots', spotRouter) 
 
-router.use('/reviews', reviewRouter) //added here
+router.use('/reviews', reviewRouter) 
 
 router.use('/bookings', bookingRouter)
 
@@ -28,8 +26,5 @@ router.use('/spot-images', spotImageRouter)
 
 router.use('/review-images', reviewImageRouter)
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
 
 module.exports = router;
