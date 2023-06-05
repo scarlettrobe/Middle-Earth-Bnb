7425182c797e21fd2f132f4d144d694b59963eb4
Kim <kimharris2018@gmail.com>
2023-06-05 16:44:56 -0500

    Reverting

--- backend/db/seeders/20230515202749-demo-user.js
+++ backend/db/seeders/20230515202749-demo-user.js
@@ -11,145 +11,27 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
-        firstName: 'Gandalf',
-        lastName: 'the Grey',
-        email: 'gandalf@user.io',
-        username: 'greywizard',
-        hashedPassword: bcrypt.hashSync('mellon', 10)
+        firstName: 'Kim',
+        lastName: 'Harris',
+        email: 'kimharris@user.io',
+        username: 'scarlettrobe',
+        hashedPassword: bcrypt.hashSync('123456', 10)
       },
       {
-        firstName: 'Bilbo',
-        lastName: 'Baggins',
-        email: 'bilbo@user.io',
-        username: 'burglar',
-        hashedPassword: bcrypt.hashSync('shire123', 10)
+        firstName: 'Melanie',
+        lastName: 'Martinez',
+        email: 'plutodestroy@user.io',
+        username: 'Crybaby14',
+        hashedPassword: bcrypt.hashSync('portals2', 10)
       },
       {
-        firstName: 'Gimli',
-        lastName: 'Son of Gloin',
-        email: 'gimli@user.io',
-        username: 'dwarfwarrior',
-        hashedPassword: bcrypt.hashSync('axe123', 10)
+        firstName: 'Andrew',
+        lastName: 'Sliva',
+        email: 'buymygame@user.io',
+        username: 'fableforge',
+        hashedPassword: bcrypt.hashSync('Voxelcube12', 10)
       },
-      {
-        firstName: 'Legolas',
-        lastName: 'Greenleaf',
-        email: 'legolas@user.io',
-        username: 'elfarcher',
-        hashedPassword: bcrypt.hashSync('arrow456', 10)
-      },
-      {
-        firstName: 'Aragorn',
-        lastName: 'Son of Arathorn',
-        email: 'aragorn@user.io',
-        username: 'rangerking',
-        hashedPassword: bcrypt.hashSync('anduril', 10)
-      },
-      {
-        firstName: 'Frodo',
-        lastName: 'Baggins',
-        email: 'frodo@user.io',
-        username: 'ringbearer',
-        hashedPassword: bcrypt.hashSync('theonering', 10)
-      },
-      {
-        firstName: 'Samwise',
-        lastName: 'Gamgee',
-        email: 'sam@user.io',
-        username: 'faithfulfriend',
-        hashedPassword: bcrypt.hashSync('potatostew', 10)
-      },
-      {
-        firstName: 'Peregrin',
-        lastName: 'Took',
-        email: 'pippin@user.io',
-        username: 'foolofatook',
-        hashedPassword: bcrypt.hashSync('pipeweed', 10)
-      },
-      {
-        firstName: 'Meriadoc',
-        lastName: 'Brandybuck',
-        email: 'merry@user.io',
-        username: 'brandybuck',
-        hashedPassword: bcrypt.hashSync('brandybuck1', 10)
-      },
-      {
-        firstName: 'Boromir',
-        lastName: 'of Gondor',
-        email: 'boromir@user.io',
-        username: 'manofgondor',
-        hashedPassword: bcrypt.hashSync('gondor123', 10)
-      },
-      {
-        firstName: 'Sauron',
-        lastName: 'the Deceiver',
-        email: 'sauron@mordor.com',
-        username: 'darklord',
-        hashedPassword: bcrypt.hashSync('oneRing', 10)
-      },
-      {
-        firstName: 'Saruman',
-        lastName: 'the White',
-        email: 'saruman@isengard.com',
-        username: 'whitewizard',
-        hashedPassword: bcrypt.hashSync('orthanc123', 10)
-      },
-      {
-        firstName: 'Galadriel',
-        lastName: 'of Lorien',
-        email: 'galadriel@lorien.com',
-        username: 'elvenlady',
-        hashedPassword: bcrypt.hashSync('nimloth', 10)
-      },
-      {
-        firstName: 'Thorin',
-        lastName: 'Oakenshield',
-        email: 'thorin@erebor.com',
-        username: 'dwarfking',
-        hashedPassword: bcrypt.hashSync('erebor', 10)
-      },
-      {
-        firstName: 'Eowyn',
-        lastName: 'of Rohan',
-        email: 'eowyn@rohan.com',
-        username: 'shieldmaiden',
-        hashedPassword: bcrypt.hashSync('rohirrim', 10)
-      },
-      {
-        firstName: 'Theoden',
-        lastName: 'King of Rohan',
-        email: 'theoden@rohan.com',
-        username: 'rohanking',
-        hashedPassword: bcrypt.hashSync('edoras123', 10)
-      },
-      {
-        firstName: 'Elrond',
-        lastName: 'Half-elven',
-        email: 'elrond@rivendell.com',
-        username: 'rivendell',
-        hashedPassword: bcrypt.hashSync('vilya', 10)
-      },
-      {
-        firstName: 'Treebeard',
-        lastName: 'Ent',
-        email: 'treebeard@fangorn.com',
-        username: 'entguardian',
-        hashedPassword: bcrypt.hashSync('entmoot', 10)
-      },
-      {
-        firstName: 'Faramir',
-        lastName: 'of Gondor',
-        email: 'faramir@gondor.com',
-        username: 'captainofithilien',
-        hashedPassword: bcrypt.hashSync('ithilien123', 10)
-      },
-      {
-        firstName: 'Gollum',
-        lastName: 'Smeagal',
-        email: 'gollum@user.io',
-        username: 'precious',
-        hashedPassword: bcrypt.hashSync('myprecious', 10)
-      },
+
     ], {});
   },
 
@@ -157,16 +39,7 @@ down: async (queryInterface, Sequelize) => {
   options.tableName = 'Users';
   const Op = Sequelize.Op;
   return queryInterface.bulkDelete(options, {
-    username: {
-      [Op.in]: [
-        'greywizard', 'burglar', 'dwarfwarrior', 'elfarcher',
-        'rangerking', 'ringbearer', 'faithfulfriend', 'foolofatook',
-        'brandybuck', 'manofgondor', 'darklord', 'whitewizard',
-        'elvenlady', 'dwarfking', 'shieldmaiden', 'rohanking',
-        'rivendell', 'entguardian', 'captainofithilien', 'precious'
-      ]
-    }
+    username: { [Op.in]: ['scarlettrobe', 'Crybaby14', 'fableforge'] }
   }, {});
-
 }
 };
---
+++ frontend/src/components/Spots/SpotShow.js
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+export const SpotShow = () => {
+    const { spotId } = useParams();
+
+    return (
+        <>
+        </>
+    )
+}
